5012e65cd61b9e99048fdaba2b23f3ba38222742
jaime h <jaimeestudiante1@gmail.com>
2016-04-11 15:56:29 -0500

    create the new app linktrackers this is an example

---
+++ linktrackers/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from linktrackers.models import Publicacion
+from linktrackers.models import link
+
+# Register your models here.
+admin.site.register(Publicacion)
+admin.site.register(link)
