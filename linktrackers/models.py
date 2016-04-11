78c00decd8e84666152c2bc56abd078a677603f7
jaime h <jaimeestudiante1@gmail.com>
2016-04-11 15:57:15 -0500

    add new model of database and impor in the file admin.py

--- linktrackers/models.py
+++ linktrackers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
@@ -6,6 +7,21 @@ from django.db import models
 # tener en cuenta los tipos de datos y la forma en la que se declaran
 # tener en cuidado con las constrains
 
-class link(models.model):
-    link_description = models.CharField(max_length=200).db_column
-    link_url = models.CharField(max_length=200).primary_key.db_column
+# table links
+class link(models.Model):
+    link_description = models.TextField(primary_key=True,serialize=True)
+    link_url = models.TextField(unique=True)
+
+    class Meta:
+        db_table = "links"
+
+
+class Publicacion(models.Model):
+    idPubli = models.AutoField(primary_key=True)
+    contenidoPubli = models.TextField(max_length=300)
+    numeroLikesPubli = models.IntegerField(max_length=99)
+    fechaPubli = models.CharField(max_length=20)
+    publicadorPubli = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    class Meta:
+        db_table = "publicacion"
