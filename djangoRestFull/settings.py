7a2ad823f0e90a30de1896b12b86b086617ae104
jaime h <jaimeestudiante1@gmail.com>
2016-04-11 15:59:56 -0500

    add new app in the settings.py in method of installed_apps and comment line 45 this is the configuration of csrf (fireware ) I enough't sure

--- djangoRestFull/settings.py
+++ djangoRestFull/settings.py
@@ -36,12 +36,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'linktrackers',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+   # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -57,8 +58,12 @@ WSGI_APPLICATION = 'djangoRestFull.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'python',
+        'USER':'jaime',
+        'PASSWORD':'jaime',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
